52cd1797267cefebdb27461a69ceea135984a9a1
Tran Linh <tranlinh265@gmail.com>
2016-11-05 23:57:33 +0700

    Use SSL and the Puma webserver in production

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact', as: 'contact'
 
   get 'signup' => 'users#new', as: 'signup'
+  post 'signup' => 'users#create'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
